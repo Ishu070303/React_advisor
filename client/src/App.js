27ce0ab1188f0d6c1e33e5a1ae04de1fa607b1f9
Eshika Rawat <eshikarawat02@gmail.com>
2022-05-11 11:40:59 +0530

    Added class component and fetch func. using axios

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './App.css';
+
+class App extends React.Component{
+    state = { advice : '' };
+    
+    componentDidMount(){
+     
+    }
+
+    render(){
+        return(
+            <div>
+                App
+            </div>
+        )
+    }
+};
+
+export default App;
